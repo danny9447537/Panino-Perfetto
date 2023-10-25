0772602a31e0fdac6bb35691f07b5b00c14d91f3
danny9447537 <danny8019166645@gmail.com>
2023-10-25 17:03:19 -0400

    1st iteration

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* Preload */
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  this.document.body.classList.add("loaded");
+});
