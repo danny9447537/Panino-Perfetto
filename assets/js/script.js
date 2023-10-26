c56541fe318cbc98d0ad8825dac275979dcdb62e
danny9447537 <danny8019166645@gmail.com>
2023-10-25 21:55:31 -0400

    3rd iteration

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,17 @@ window.addEventListener("load", function () {
   preloader.classList.add("loaded");
   this.document.body.classList.add("loaded");
 });
+
+/* Adding an event listener on multiple elements */
+
+const addEventOnAllElements = function (elements, eventType, callback) {
+  len = elements.length;
+  let i;
+  for (i = 0; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+/* Navbar */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelector("[data-nav-toggler]");
